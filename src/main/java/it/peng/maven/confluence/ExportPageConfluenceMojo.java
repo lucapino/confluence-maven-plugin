7d53181ade095c85e1751eee788fc978f4121dab
Luca Tagliani <l.tagliani@cbt.it>
2014-05-30 17:45:57 +0200

    Updated dependencies. Changed groupId to com.github.lucapino for OSS releasing.

--- src/main/java/it/peng/maven/confluence/ExportPageConfluenceMojo.java
+++ src/main/java/it/peng/maven/confluence/ExportPageConfluenceMojo.java
@@ -19,6 +19,7 @@ package it.peng.maven.confluence;
 import it.peng.maven.confluence.model.PageDescriptor;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
@@ -26,9 +27,10 @@ import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.wagon.authentication.AuthenticationInfo;
 
@@ -102,7 +104,7 @@ public class ExportPageConfluenceMojo extends AbstractConfluenceMojo {
     private void downloadFile(HttpGet request) throws MojoFailureException {
         InputStream in = null;
         FileOutputStream out = null;
-        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpClient httpClient = HttpClientBuilder.create().build();
         try {
             HttpResponse response = httpClient.execute(request);
             if (response == null || response.getEntity() == null) {
@@ -112,7 +114,9 @@ public class ExportPageConfluenceMojo extends AbstractConfluenceMojo {
                 out = new FileOutputStream(outputFile);
                 IOUtils.copy(in, out);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
+            throw fail("Unable to download page", e);
+        } catch (IllegalStateException e) {
             throw fail("Unable to download page", e);
         } finally {
             IOUtils.closeQuietly(in);
